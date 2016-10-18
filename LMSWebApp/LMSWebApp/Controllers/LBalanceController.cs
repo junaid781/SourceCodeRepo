6107dd5eb696d841d1379d619480d5272aa87dfd
Hamza Rahimy <Hamza Rahimy>
2016-10-18 17:45:34 +0400

    Linked the Leave History page with layout

---
+++ LMSWebApp/LMSWebApp/Controllers/LBalanceController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMSWebApp.Controllers
+{
+    public class LBalanceController : Controller
+    {
+        //
+        // GET: /LBalance/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
